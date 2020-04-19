fde0ab64acbbb7e1602e54625ded8266b064de40
Na Chen <nchen312@gmail.com>
2020-04-19 11:52:38 -0400

    add users

--- public/user/sketch.js
+++ public/user/sketch.js
@@ -1,10 +1,10 @@
 socket = io('/user');
 userData = {}
 
-socket.on('connect', function () {
+socket.on('connect', function() {
   console.log("Connected", socket.id);
 
-  socket.on('setCredentials', function (credentials) {
+  socket.on('setCredentials', function(credentials) {
     // credentials format
     // {
     //   role: String (USER or AUDIENCE)
@@ -15,7 +15,8 @@ socket.on('connect', function () {
     userData = credentials
   });
 
-  socket.on('message', function (message) {
+  socket.on('message', function(message) {
+    $('.chat').append('<p><strong>' + data.user + '</strong>: ' + data.message + '</p>');
     // message format
     // {
     //   text: String
@@ -29,6 +30,17 @@ socket.on('connect', function () {
 });
 
 // TODO: actually fill the text from a user input
+
+let input;
+input = select('#message_input');
+
+
+function keyPressed() {
+  if (keyCode == ENTER) {
+    input.value('');
+  }
+}
+
 function sendMessage() {
   socket.emit('message', {
     text: 'the quick brown dog jumped over the lazy fox',
